67508da24c25bb54914f9810d71ca33b6c708950
Pranshu <Pranshu Malviya>
2018-03-07 23:52:00 +0530

    SQL

--- src/main/java/com/example/arangospark/temp/App.java
+++ src/main/java/com/example/arangospark/temp/App.java
@@ -22,19 +22,19 @@ public class App {
 	}
 	
 	public static void main(String[] args) throws IOException {
-		String chart="2",check_build="",s="";
-//		check_build = args[0];
-//		if (check_build.equals("0")) {
-//			try {
-//				s = args[1];
-//				buildGraph(USER,PASSWD,s);
-//			} catch (Exception e) {
-//				System.err.println("Build error");
-//			}
-//		}
-//		else {
-//			chart = args[0];
-//			s = args[1];
+		String chart="",check_build="",s="";
+		check_build = args[0];
+		if (check_build.equals("0")) {
+			try {
+				s = args[1];
+				buildGraph(USER,PASSWD,s);
+			} catch (Exception e) {
+				System.err.println("Build error");
+			}
+		}
+		else {
+			chart = args[0];
+			s = args[1];
 			SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("arango")	
 	                .set("arangodb.host", "127.0.0.1")
 	                .set("arangodb.port", "8529")
@@ -43,8 +43,7 @@ public class App {
 	        JavaSparkContext sc = new JavaSparkContext(conf);
 	        ArangoJavaRDD<Node> rddv = ArangoSpark.load(sc, VERTEXT_COLLECTION_NAME, Node.class);
 	        ArangoJavaRDD<Link> rdde = ArangoSpark.load(sc, EDGE_COLLECTION_NAME, Link.class);
-	        new Analysis(rddv,rdde,chart,s);
-	        
-//		}
+	        new Analysis(rddv,rdde,chart,s);   
+		}
     }
 }
