59fd9af70c4bae11bc8b2cdd5cd5072365264816
Pranshu Malviya <9971260+pranshu28@users.noreply.github.com>
2018-03-15 16:58:38 +0530

    Update Link.java

--- src/main/java/com/example/arangospark/temp/Link.java
+++ src/main/java/com/example/arangospark/temp/Link.java
@@ -5,67 +5,82 @@ import java.io.Serializable;
 import com.arangodb.entity.DocumentField;
 import com.arangodb.entity.DocumentField.Type;
 
-public class Link implements Serializable{
+public class Node implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@DocumentField(Type.ID)
 	private String _id;
-
+	
 	@DocumentField(Type.KEY)
 	private String _key;
-
+	
 	@DocumentField(Type.REV)
 	private String _revision;
-
-	@DocumentField(Type.FROM)
-	private String _from;
-
-	@DocumentField(Type.TO)
-	private String _to;
-
-	private String add_to_cart_order,reordered;
+	    
+	private String eval_set,order_number,order_dow,order_hour_of_day,days_since_prior_order,aisle,department,product_name;
 	
-	public Link() {}
+	public Node(String label) {
+	    this._key = label;
+	}
+	public Node() {}
 	
-    public Link(String _from, String _to, String label) {
-        this._from = _from;
-        this._to = _to;
-        this._key = label;
-    }
-    
-	public String getreordered() {
-		return this.reordered;
+	public boolean isOrder() {
+		try {
+			if (this.order_hour_of_day != null) {
+				return true;
+	    	}
+	    } catch (Exception e) {
+	    	return false;
+	   	}
+	   	return false;
 	}
-	public void setreordered(String reordered) {
-		this.reordered = reordered;
+	public String getproduct_name() {
+		return this.product_name;
 	}
-	public String getadd_to_cart_order() {
-		return this.add_to_cart_order;
+	public void setproduct_name(String product_name) {
+		this.product_name = product_name;
 	}
-	public void setadd_to_cart_order(String add_to_cart_order) {
-		this.add_to_cart_order = add_to_cart_order;
+	public String getdepartment() {
+		return this.department;
 	}
-	public String getFrom() {
-        return _from;
-    }
-    public String getTo() {
-        return _to;
-    }
-    public boolean hasProp() {
-    	try {
-    		if (this.reordered != null || this.add_to_cart_order != null) {
-    			return true;
-    		}
-    	} catch (Exception e) {
-    		return false;
-    	}
-    	return false;
-    }
-	public void set_from(String newFrom) {
-		this._from = newFrom;
+	public void setdepartment(String department) {
+		this.department = department;
 	}
-	public void set_to(String newTo) {
-		this._to = newTo;
+	public String getaisle() {
+		return this.aisle;
+	}
+	public void setaisle(String aisle) {
+		this.aisle = aisle;
+	}
+	public String getdays_since_prior_order() {
+		return this.days_since_prior_order;
+	}
+	public void setdays_since_prior_order(String days_since_prior_order) {
+		this.days_since_prior_order = days_since_prior_order;
+	}
+	public String getorder_hour_of_day() {
+		return this.order_hour_of_day;
+	}
+	public void setorder_hour_of_day(String order_hour_of_day) {
+		this.order_hour_of_day = order_hour_of_day;
+	}
+	public String getorder_dow() {
+		return this.order_dow;
+	}
+	public void setorder_dow(String order_dow) {
+		this.order_dow = order_dow;
+	}
+	public String geteval_set() {
+		return this.eval_set;
+	}
+	public void setorder_number(String order_number) {
+		this.order_number = order_number;
+	}
+	public String getorder_number() {
+		return this.order_number;
+	}
+	public void seteval_set(String eval_set) {
+		this.eval_set = eval_set;
 	}
 	public String getId() {
 		return this._id;
